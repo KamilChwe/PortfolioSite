d892573633aed1b44f88ac94d7c5ada9e7f5064e
Kamil Chwedura <kchwedura@gmail.com>
2024-12-02 23:52:23 +0000

    Added Projects
    - Added Projects to the Projects Tab
    - Added a Scroll to Top button
    - Modified the scrollbar
    - Minor About Me changes
    - GitHub added to Socials

---
+++ Portfolio/js/functions.js
@@ -0,0 +1,19 @@
+let scrollButton = document.getElementById("scrollButton");
+
+window.onscroll = function(){
+    scrollFunction();
+};
+
+function scrollFunction(){
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollButton.style.display = "block";
+    }
+    else{
+        scrollButton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
